3e170806d6e8396036f945b28392ad03990dc094
Tarcísio Anunciação <tarcisio@vasta.me>
2025-03-15 22:35:30 -0300

    Ajusta configuração do Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/neu-figma/',
   plugins: [],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 });
